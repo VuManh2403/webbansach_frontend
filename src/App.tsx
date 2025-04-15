62c5a6a3b47ca521d3886078876c32f5ce5d8c06
VuManh2403 <vdm24032002@gmail.com>
2025-04-15 14:42:53 +0700

    List

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Navbar from "./layouts/header-footer/Navbar";
 import Footer from "./layouts/header-footer/Footer";
 import HomePage from "./layouts/homepage/HomePage";
 
-
 function App() {
   return (
     <div className="App">
